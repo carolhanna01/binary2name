b815ceb6a65e4faa06451383a07ad77c125ba288
Carol Hanna <carolhanna@rishon.csf.technion.ac.il>
2020-09-11 17:03:53 +0300

    adding more efficient base main

--- paths_only_main.py
+++ paths_only_main.py
@@ -132,16 +132,18 @@ def tokenize_function_name(function_name):
 
 
 
-def generate_dataset(train_binaries, dataset_name): #keep
-
-    dataset_dir = f"datasets/{dataset_name}"
-    os.makedirs(dataset_dir, exist_ok=True)
-    analysed_funcs = get_analysed_funcs(dataset_dir)
+def generate_dataset(train_binaries, output_name, dataset_name): #keep
+    
+    usable_functions_file = open("our_dataset/"+ dataset_name + "/usable_functions_names.txt", "r")
+    usable_functions = [name.strip() for name in usable_functions_file]
+    output_dir = f"datasets/{output_name}"
+    os.makedirs(output_dir, exist_ok=True)
+    analysed_funcs = get_analysed_funcs(output_dir)
     for binary in train_binaries:
-        analysed_funcs = analyse_binary(analysed_funcs, binary, dataset_dir)
+        analysed_funcs = analyse_binary(analysed_funcs, binary, output_dir, usable_functions)
 
 
-def analyse_binary(analysed_funcs, binary_name, dataset_dir): #keep
+def analyse_binary(analysed_funcs, binary_name, dataset_dir, usable_functions): #keep
     excluded = {'main', 'usage', 'exit'}.union(analysed_funcs)
     proj = angr.Project(binary_name, auto_load_libs=False)
     cfg = proj.analyses.CFGFast()
@@ -151,8 +153,8 @@ def analyse_binary(analysed_funcs, binary_name, dataset_dir): #keep
     funcs = get_cfg_funcs(proj, binary_name, excluded)
     print(f"{binary_name} have {len(funcs)} funcs")
     for test_func in funcs:
-        if test_func.name in analysed_funcs:
-            print(f"skipping {test_func.name}")
+        if (test_func.name in analysed_funcs) or (tokenize_function_name(test_func.name) not in usable_functions):
+            print(f"skipping {tokenize_function_name(test_func.name)}")
             continue
         print(f"analyzing {binary_name}/{test_func.name}")
         output = open(os.path.join(binary_dir, f"{test_func.name}"), "w")
@@ -245,191 +247,16 @@ def sm_to_output(sm: angr.sim_manager.SimulationManager, output_file, func_name)
                 skipped_lines += 1
     print(f"skipped {skipped_lines} lines")
 
-
-def num_in_sets(set_counts):
-    return set_counts['train'] + set_counts['val'] + set_counts['test']
-
-
-def update_hist(func_hist, name_parts, set):
-    for func in name_parts:
-        func_counts = func_hist[func]
-        func_counts['free'] -= 1
-        func_counts[set] += 1
-    return func_hist
-
-
-def set_decide(func_hist, name_parts, global_counters): #keep
-    """
-    here we tried to devide the inputs between the train/val/test sets such that there is more shared names between the
-    sets
-    :param func_hist: counters for each name, how many times it appeared in each set
-    :param name_parts: names that consist the function name ( '_' seperated function name)
-    :return: set to place this function in
-    """
-    min_func = name_parts[0]
-    min_in_set = num_in_sets(func_hist[min_func])
-    for func in name_parts:
-        if func not in func_hist:
-            continue
-        curr_in_set = num_in_sets(func_hist[func])
-        if curr_in_set < min_in_set:
-            if curr_in_set != min_in_set or func_hist[func]['free'] > func_hist[min_func]['free']:
-                continue
-            min_func = func
-            min_in_set = curr_in_set
-
-    min_counts = func_hist[min_func]
-    if min_counts['train'] == 0:
-        return update_hist(func_hist, name_parts, 'train'), 'train'
-    if min_counts['val'] == 0:
-        return update_hist(func_hist, name_parts, 'val'), 'val'
-    if min_counts['test'] == 0:
-        return update_hist(func_hist, name_parts, 'test'), 'test'
-
-    total_samples = sum(global_counters.values())
-    if global_counters['train'] / total_samples < 0.7:
-        return update_hist(func_hist, name_parts, 'train'), 'train'
-    elif global_counters['val'] / total_samples < 0.2:
-        return update_hist(func_hist, name_parts, 'val'), 'val'
-    else:
-        return update_hist(func_hist, name_parts, 'test'), 'test'
-
-
-def gen_shared_name(func_hist, funcs):
-    shared_funcs = []
-    for func in funcs:
-        if func in func_hist:
-            shared_funcs.append(func)
-    return shared_funcs
-
-
-def generate_output(dataset_path, dataset_name): #keep
-    """
-    this is the experimentation code at the last experiments, we tried to add to the test/val sets only functions that
-    have a name part the appeared at least 3 times in the dataset, later we tried to remove from the label the name parts
-    that didn't appear more than 3 times, and wrote a function that divides the training functions in a way that
-    promotes sharing names across train/val/test sets
-    """
-    def func_name_extractor(x):
-        x = os.path.basename(x)
-        if x.endswith(".pkl"):
-            return x[:-len(".pkl")]
-        return x
-
-    binaries = list(os.scandir(dataset_path))
-    import numpy as np
-    np.random.seed(42)
-    np.random.shuffle(binaries)
-    train_output = open(os.path.join(dataset_path, dataset_name + "_train_output.txt"), "w")
-    test_output = open(os.path.join(dataset_path, dataset_name + "_test_output.txt"), "w")
-    val_output = open(os.path.join(dataset_path, dataset_name + "_val_output.txt"), "w")
-    mapper = dict()
-    all_funcs = set()
-    for i, entry in enumerate(binaries):
-        funcs = list(glob(f"{entry.path}/*"))
-        all_funcs.update(funcs)
-        for func in funcs:
-            func_name = func_name_extractor(func)
-            func_name = func_name.split("_")
-            for label in func_name:
-                if label not in mapper:
-                    mapper[label] = []
-                mapper[label].append(func)
-
-    well_named_funcs = set()
-    popular_names = filter(lambda x: len(x[1]) >= 3, mapper.items())
-    
-    count_func_names = open(os.path.join(dataset_path, "count_func_names.txt"), "w")
-    for name, name_funcs in mapper.items():
-        line= name + " " + str(len(name_funcs)) + "\n"
-        count_func_names.write(line)
-    
-
-    names_hists = {name: {'free': len(name_funcs), 'train': 0, 'val': 0, 'test': 0} for name, name_funcs in popular_names}
-    for partial in map(lambda x: x[1], filter(lambda x: len(x[1]) >= 3, mapper.items())):
-        well_named_funcs.update(partial)
-    well_named_funcs = list(well_named_funcs)
-
-    # generate output
-    np.random.shuffle(well_named_funcs)
-    print(f"{len(all_funcs)} functions, {len(well_named_funcs)} functions with a name that contains a common word")
-    # print("choosing 250 functions for test/validation")
-
-    global_counters = {'train': 0, 'val': 0, 'test': 0}
-    for i, func in enumerate(well_named_funcs):
-        func_name_parts = func_name_extractor(func).split("_")
-        print_name = gen_shared_name(names_hists, func_name_parts)
-        names_hists, dest = set_decide(names_hists, print_name, global_counters)
-        global_counters[dest] += 1
-        print_name = "|".join(print_name)
-        if dest == 'train':
-            output = train_output
-        elif dest == 'test':
-            output = test_output
-        else:
-            output = val_output
-
-        print(f"shared name: {print_name}")
-        all_funcs.remove(func)
-        if func.endswith(".pkl"):
-            with open(func, "rb") as f:
-                try:
-                    sm = pickle.load(f)
-                    sm_to_output(sm, output, print_name)
-                except Exception as e:
-                    print(e)
-                    print(f"{func} failed")
-        else:
-            print("pickle not found")
-            with open(func, "r") as f:
-                for line in f:
-                    line = line.split(" ")
-                    line[0] = print_name
-                    line = " ".join(line)
-                    output.write(line)
-    train_output.close()
-    test_output.close()
-    val_output.close()
-
-
 def main():
     parser = argparse.ArgumentParser()
-    # we did this in order to parallelize the analysis process
     parser.add_argument("--binary_idx", type=int, required=True)
     parser.add_argument("--dataset", type=str, required=True)
+    parser.add_argument("--output", type=str, required=True)
     args = parser.parse_args()
-    binaries = os.listdir("coreutils_bins")
-    #print("Hi , main after args")
+    binaries = os.listdir("our_dataset/"+ args.dataset)
     binaries.sort()
-    binaries = [f"coreutils_bins/{binary}" for binary in binaries]
-    generate_dataset([binaries[args.binary_idx]], args.dataset)
-    #print("successfully exited")
-    generate_output("datasets/" + args.dataset, args.dataset)
-
-
-def trim_long_lines(file_path):
-    file = open(file_path, "r")
-    output_path = os.path.join(os.path.dirname(file_path), f"trimmed_{os.path.basename(file_path)}")
-    output_file = open(output_path, "w")
-    max_100_lengths = [0] * 100
-    counter = 0
-    lengths = []
-
-    for line in file:
-        curr_min = min(max_100_lengths)
-        lengths.append(len(line))
-        if len(line) > 2500:
-            counter += 1
-        else:
-            output_file.write(line)
-        if len(line) > curr_min:
-            max_100_lengths[max_100_lengths.index(curr_min)] = len(line)
-    max_100_lengths.sort(reverse=True)
-    print(max_100_lengths)
-    print(counter)
-    import matplotlib.pyplot as plt
-    plt.hist(lengths, bins='auto')
-    plt.show()
+    binaries = [f"our_dataset/" + args.dataset+ f"/{binary}" for binary in binaries]
+    generate_dataset([binaries[args.binary_idx]], args.output, args.dataset)
 
 
 if __name__ == '__main__':
